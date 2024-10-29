9ab8f1d5c6ac5c4aabe864ee41c152faaa5e3ac5
MOISES-17 <i2422095@continental.edu.pe>
2024-10-29 18:29:39 -0500

    ajuste

--- trabajo1.py
+++ trabajo1.py
@@ -1,7 +1,7 @@
 import streamlit as st
     
     #ejercicio5: generar e imprimir los numeros pares entre 0 y 100
-    st.title("ejercicio de numeros pares")
+    st.title("ejercicios de numeros pares")
 
     st.subheader("ejercicio 5: Numeros pares entre 0 y 100")
     pares_0_100 = (i for i in range(0,101) if i % 2 == 0)
