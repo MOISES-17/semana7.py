2ac97c676028807e1b061578438dd56e3be6866f
MOISES-17 <i2422095@continental.edu.pe>
2024-10-29 18:22:37 -0500

    toques

--- semana7.py
+++ semana7.py
@@ -24,15 +24,3 @@ def verificar_contrasena():
 #llamada la funcion principal
 if __name__=="__main__":
     verificar_contrasena()
-
-------------------------------------
-    import streamlit as st
-    
-    #ejercicio5: generar e imprimir los numeros pares entre 0 y 100
-    st.title("ejercicio de numeroa pares")
-
-    st.subheader("ejercicio 5: Numero pares entre 0 y 100")
-    pares_0_100 = (i for i in range(0,101) if i % 2 == 0)
-    st.write("numeros pares entre 0 y 100:")
-    st.white(pares_0_100)
-    
