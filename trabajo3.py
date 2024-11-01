86ca9c14b165b5168c5dd87502904803bdbc2162
MOISES-17 <i2422095@continental.edu.pe>
2024-11-01 09:01:19 -0500

    cambio

--- trabajo3.py
+++ trabajo3.py
@@ -6,10 +6,10 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
-def multiplicacion(a,b):
+def multiplicacion(a, b):
     return a * b
 
-def division(a,b):
+def division(a, b):
     if b != 0:
         return a/b
     else:
