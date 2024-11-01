3ab3a876476e89f976b7520d66a8323cffd5ca59
MOISES-17 <i2422095@continental.edu.pe>
2024-11-01 10:03:15 -0500

    guardar

--- trabajo1.py
+++ trabajo1.py
@@ -1,10 +1,26 @@
 import streamlit as st
-    
-    #Ejercicio5: generar e imprimir los números pares entre 0 y 100
-    st.title("Ejercicios de números pares")
+def verificar_contrasena():
+    st.title("verificacion de contraseña")
 
-    st.subheader("Ejercicio 5: Números pares entre 0 y 100")
-    pares_0_100 = (i for i in range(0,101) if i % 2 == 0)
-    st.write("Números pares entre 0 y 100:")
-    st.white(pares_0_100)
-    
+    #contraseña correcta
+    contrasena_correcta = "asdasd"
+
+    #inicializar variable de sesion para almacenar la entrada de usuario
+    if "contrasena_ingresada" not in st.session_state:
+        st.session_state.contrasena_ingresada = ""
+
+     #imput de la contraseña
+    contrasena_ingresada = st.text_input("ingrese la contraseña", type="password")
+
+    #boton para verificar la contraseña
+    if st.button("verificar"):
+        if contrasena_ingresada == contrasena_correcta:
+            st.session_state.contrasena_ingresada = contrasena_ingresada
+            st.success("bienvenido")
+        else:
+            st.error("contraseña_incorrecta, intente de nuevo")
+
+
+#llamada la funcion principal
+if __name__=="__main__":
+    verificar_contrasena()
---
+++ trabajo6.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def main():
+  """Muestra un menú con opciones de Archivo, Editar, Ver y Salir."""
+
+  st.title("Menú de Opciones")
+
+  while True:
+    opcion = st.sidebar.selectbox("Seleccione una opción:", ["Archivo", "Editar", "Ver", "Salir"])
+
+    if opcion == "Archivo":
+      st.write("**Archivo**")
+      # Aquí se puede agregar código para las opciones del menú Archivo
+    elif opcion == "Editar":
+      st.write("**Editar**")
+      # Aquí se puede agregar código para las opciones del menú Editar
+    elif opcion == "Ver":
+      st.write("**Ver**")
+      # Aquí se puede agregar código para las opciones del menú Ver
+    elif opcion == "Salir":
+      st.write("**¡Hasta luego!**")
+      break
+
+if __name__ == "__main__":
+  main()
