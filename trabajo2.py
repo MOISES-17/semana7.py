adc50ad7e43d4560786f162fafdc1322edde61e8
MOISES-17 <i2422095@continental.edu.pe>
2024-10-31 21:32:47 -0500

    cargar

--- trabajo2.py
+++ trabajo2.py
@@ -1,3 +1,24 @@
 import streamlit as st
+import math
 
-#algoritmo de numero hasta que se intruduzca cero
+def calcular_area(radio):
+    return math.pi * (radio ** 2)
+
+def calcular_perimetro(radio):
+    return 2 * math.pi * radio
+
+def main():
+    st.title("calcular del Area y Perimetro de una Circurferencia")
+
+    radio = st.number_input("ingrese el radio de la circurferencia:", min_values=0.0, step=0.1)
+    if radio > 0:
+        area = calcular_area(radio)
+        perimetro = calcular_perimetro(radio)
+
+        st.write(f"Area: {area:.2f}")
+        st.write(f"Perimetro: {perimetro:.2f}")
+    else:
+        st.write("Por favor, ingrese un radio mayor a cero.")
+
+if __name__ == "__main__":
+    main()
