d8e0c5a307d95f72d278555d3fab2c9f4f18b7d2
MOISES-17 <i2422095@continental.edu.pe>
2024-11-01 09:08:22 -0500

    cambio

--- trabajo3.py
+++ trabajo3.py
@@ -1,20 +1,15 @@
 import streamlit as st
-
 def suma(a, b):
     return a + b
-
 def resta(a, b):
     return a - b
-
 def multiplicacion(a,b):
     return a * b
-    
 def division(a,b):
     if b != 0:
         return a/b
     else:
         return "Error: División por cero"
-
 def main():
     st.title("Calculadora Básica")
     #Entrada de datos
